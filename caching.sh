254603c7b2a98f4b4daf941e63ba8392562e6fa4
humpf <humpfbold@gmail.com>
2013-01-28 17:22:13 +0100

    remove latex produced files from cache

--- caching.sh
+++ caching.sh
@@ -5,7 +5,7 @@ rm ~/.dmenufile_cache
 directories="Documents dotfiles Desktop Downloads Music Pictures scripts stuff Videos"
 
 for dir in $directories; do
-    find ~/$dir -type f | sed 's/ /\\ /g' | grep -v ".git/" >> ~/.dmenufile_cache_sort
+    find ~/$dir -type f | sed 's/ /\\ /g' | grep -v "\.git/" | grep -v "\.aux" | grep -v "\.tex" | grep -v "\.log" | grep -v "\.toc" >> ~/.dmenufile_cache_sort
 done
 find ~ -maxdepth 1 -type f | sed 's/ /\\ /g' | rev | awk -F \/ '{print $1}' | rev >> ~/.dmenufile_cache_sort
 
