ac9b437ac5cd11368ee52d0d181aafb96e82aaae
humpf <humpfbold@gmail.com>
2013-09-30 11:02:18 +0200

    moved folder to ~/.cache

--- caching.sh
+++ caching.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm ~/.dmenufile_cache
+rm ~/.cache/dmenufile_cache
 
 directories="Documents dotfiles Desktop Downloads Music Pictures scripts stuff Videos"
 
 for dir in $directories; do
-    find ~/$dir -type f | sed 's/ /\\ /g' | grep -v "\.git/" | grep -v "\.lot" |grep -v "\.out" | grep -v "\.lof" |grep -v "\.aux" | grep -v "\.tex" | grep -v "\.log" | grep -v "\.toc" >> ~/.dmenufile_cache_sort
+    find ~/$dir -type f | sed 's/ /\\ /g' | grep -v "\.git/" | grep -v "\.lot" |grep -v "\.out" | grep -v "\.lof" |grep -v "\.aux" | grep -v "\.tex" | grep -v "\.log" | grep -v "\.toc" >> ~/.cache/dmenufile_cache_sort
 done
-find ~ -maxdepth 1 -type f | sed 's/ /\\ /g' | rev | awk -F \/ '{print $1}' | rev >> ~/.dmenufile_cache_sort
+find ~ -maxdepth 1 -type f | sed 's/ /\\ /g' | rev | awk -F \/ '{print $1}' | rev >> ~/.cache/dmenufile_cache_sort
 
-sort -f ~/.dmenufile_cache_sort > ~/.dmenufile_cache
-rm ~/.dmenufile_cache_sort
+sort -f ~/.cache/dmenufile_cache_sort > ~/.cache/dmenufile_cache
+rm ~/.cache/dmenufile_cache_sort
--- file_dmenu.sh
+++ file_dmenu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-exe = `cat ~/.dmenufile_cache | dmenu -i -nb '#63B8FF' -sb '#EE4000' -nf '#000000' -fn '-*-*-*-*-*-*-16-*-*-*-*-*-*-*' | xargs xdg-open`
+exe = `cat ~/.cache/dmenufile_cache | dmenu -i -nb '#63B8FF' -sb '#EE4000' -nf '#000000' -fn '-*-*-*-*-*-*-16-*-*-*-*-*-*-*' | xargs xdg-open`
 
 exec $exe
