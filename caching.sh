9185646401dc07d1f501ad8c86351feef910eb4a
humpf <humpfbold@gmail.com>
2014-02-24 16:57:16 +0100

    dmeun password copy

--- caching.sh
+++ caching.sh
@@ -6,7 +6,7 @@ directories="Documents dotfiles Desktop Downloads Music Pictures scripts stuff V
 
 for dir in $directories; do
     find ~/$dir -type f | sed 's/ /\\ /g' | grep -v "\.git/" | grep -v "\.lot" |grep -v "\.out" | grep -v "\.lof" |grep -v "\.aux" | grep -v "\.tex" | grep -v "\.log" | grep -v "\.toc" >> ~/.cache/dmenufile_cache_sort
-done
+dne
 find ~ -maxdepth 1 -type f | sed 's/ /\\ /g' | rev | awk -F \/ '{print $1}' | rev >> ~/.cache/dmenufile_cache_sort
 
 sort -f ~/.cache/dmenufile_cache_sort > ~/.cache/dmenufile_cache
---
+++ dmenu-pass.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+passdir=~/.password-store
+
+site=$(find $passdir -name "*.gpg" | awk -F \/ '{print $NF}' | sed "s/.gpg//"| dmenu)
+pass -c $site
